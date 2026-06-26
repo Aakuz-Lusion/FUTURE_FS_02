590eec96290c6a6337338f4d19fa9d56b797d954
Aakuz <lastkingkhan786@email.com>
2026-06-26 14:29:54 +0545

    Update production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://future-fs-02-q7bv.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
